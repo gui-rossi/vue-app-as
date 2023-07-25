a23813b0ea21604c82fc713d6a807ea01454aa8c
guirossi <guizo.rossi@gmail.com>
2023-07-25 19:59:56 -0300

    added foreground support

--- app/src/main/java/com/example/vueapp/MainActivity.kt
+++ app/src/main/java/com/example/vueapp/MainActivity.kt
@@ -1,19 +1,100 @@
 package com.example.vueapp
 
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.app.job.JobInfo.PRIORITY_MIN
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.util.Log
 import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
+import androidx.annotation.Nullable
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
 import com.example.vueapp.ui.theme.VueAppTheme
 
+
+class SleepAppService : Service() {
+    /*
+    @Nullable
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // do your jobs here
+        return super.onStartCommand(intent, flags, startId)
+    }
+    */
+
+    private val NOTIF_ID = 1
+    private val NOTIF_CHANNEL_ID = "Channel_Id"
+
+    @Nullable
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        // do your jobs here
+        startForeground()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForeground() {
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "My Background Service")
+            } else {
+                // If earlier version channel ID is not used
+                // https://developer.android.com/reference/android/support/v4/app/NotificationCompat.Builder.html#NotificationCompat.Builder(android.content.Context)
+                ""
+            }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId )
+        val notification = notificationBuilder.setOngoing(true)
+            .setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setPriority(PRIORITY_MIN)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .build()
+        startForeground(101, notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        startService(Intent(this, SleepAppService::class.java))
+
+        this.addDelay();
 
         var mWebview = WebView(this);
 
@@ -22,6 +103,13 @@ class MainActivity : ComponentActivity() {
 
         mWebview.loadUrl("https://gui-rossi.github.io/");
         setContentView(mWebview);
+    }
+
+    fun addDelay() {
+        Handler().postDelayed({
+            Log.d("TAG", "ENTREI");
+            this.addDelay();
+        }, 2000)
     }
 }
 
